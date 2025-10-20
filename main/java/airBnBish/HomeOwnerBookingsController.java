98db0da061ad18cb797000d995923922037813ab
UHMONOre <mporasalex@gmail.com>
2025-10-20 15:35:50 +0300

    Made bookings editing (Container still unfinished)

--- main/java/airBnBish/HomeOwnerBookingsController.java
+++ main/java/airBnBish/HomeOwnerBookingsController.java
@@ -1,10 +1,13 @@
 package airBnBish;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,22 +27,37 @@ public class HomeOwnerBookingsController {
     @FXML
     private VBox bookingVbox;
 
-    public void initData(Customer customer, Home home, LocalDate startDate, LocalDate endDate) throws SQLException {
+    public void initData(Customer customer, Home home, LocalDate startDate, LocalDate endDate) throws SQLException, IOException {
         this.customer = customer;
         this.home = home;
         this.startDate = startDate;
         this.endDate = endDate;
 
         HashMap<Customer , TreeSet<LocalDate>> bookingsMap = new HashMap<>();
-        TreeSet<LocalDate> bookingsDate = new TreeSet<>();
+        TreeSet<LocalDate> bookingsDateHolder = new TreeSet<>();
 
         DBmanager dBmanager = DBmanager.createConnection();
-        String sql = "select * from homebookings where HomeId = ?";
-        PreparedStatement ps = dBmanager.connection.prepareStatement(sql,
-                ResultSet.TYPE_SCROLL_INSENSITIVE,
-                ResultSet.CONCUR_READ_ONLY);
-        ps.setInt(1, home.getId());
-        ResultSet set = ps.executeQuery();
+        ResultSet set;
+
+        if (startDate == null && endDate == null){
+            String sql = "select * from homebookings where HomeId = ?";
+
+            PreparedStatement ps = dBmanager.connection.prepareStatement(sql,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            ps.setInt(1, home.getId());
+            set = ps.executeQuery();
+        } else {
+            String sql = "select * from homebookings where HomeId = ? and BookingDate >= ? and BookingDate <= ?";
+
+            PreparedStatement ps = dBmanager.connection.prepareStatement(sql,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            ps.setInt(1, home.getId());
+            ps.setDate(2, Date.valueOf(startDate));
+            ps.setDate(3, Date.valueOf(endDate));
+            set = ps.executeQuery();
+        }
 
         while (set.next()){
             bookingVbox.getChildren().remove(bookingLabel);
@@ -47,19 +65,31 @@ public class HomeOwnerBookingsController {
 
             Date sqlDate = set.getDate("BookingDate");
             LocalDate date = sqlDate.toLocalDate();
-            bookingsDate.add(date);
+            bookingsDateHolder.add(date);
 
             if (set.next()){
                 Integer nextCustomerId = set.getInt("CustomerId");
                 set.previous();
 
                 if (bookedCustomer.getId() != nextCustomerId){
-                    bookingsMap.put(bookedCustomer, new TreeSet<>(bookingsDate));
-                    bookingsDate.clear();
+                    bookingsMap.put(bookedCustomer, new TreeSet<>(bookingsDateHolder));
+                    bookingsDateHolder.clear();
                 }
             }else {
-                bookingsMap.put(bookedCustomer, new TreeSet<>(bookingsDate));
+                bookingsMap.put(bookedCustomer, new TreeSet<>(bookingsDateHolder));
             }
+        }
+
+        for (Customer bookedCustomer : bookingsMap.keySet()){
+            TreeSet<LocalDate> bookingDates = bookingsMap.get(bookedCustomer);
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("OwnerBookingContainer.fxml"));
+            AnchorPane bookingCard = loader.load();
+
+            OwnerBookingContainerController controller = loader.getController();
+            controller.initData(customer, home, bookingDates);
+
+            bookingVbox.getChildren().add(bookingCard);
         }
     }
 }
--- main/java/airBnBish/OwnerBookingContainerController.java
+++ main/java/airBnBish/OwnerBookingContainerController.java
@@ -1,5 +1,18 @@
 package airBnBish;
 
-public class OwnerBookingContainerController {
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.TreeSet;
 
+public class OwnerBookingContainerController {
+    private Customer customer;
+    private Home home;
+    private TreeSet<LocalDate> bookingDates;
+
+    public void initData(Customer customer, Home home, TreeSet<LocalDate> bookingDates){
+        this.customer = customer;
+        this.home = home;
+        this.bookingDates = bookingDates;
+        System.out.println(customer.getId());
+    }
 }
